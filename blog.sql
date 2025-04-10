9405dcd86ed5909fdf13f0a744a1806a8324eba4
AakashSinghCS <ads2004mayads@gmail.com>
2025-04-10 13:09:31 -0700

    testing admin hash

--- blog.sql
+++ blog.sql
@@ -66,7 +66,7 @@ CREATE TABLE post_categories (
 -- Insert dummy users (admin and registered users)
 INSERT INTO users (username, full_name, email, password_hash, role, profile_image)
 VALUES
-  ('admin1', 'Admin User', 'admin@example.com', 'dummyhashadmin', 'admin', '/images/admin1.png'),
+  ('admin1', 'Admin User', 'admin@example.com', '$2y$10$lj9cbtWMvcU5nMIqAOodPuHI6CuZykwerAQDYIttJ.KcoKrNktgOG', 'admin', '/images/admin1.png'),
   ('johndoe', 'John Doe', 'john@example.com', 'dummyhashjohn', 'registered', '/images/johndoe.png'),
   ('janedoe', 'Jane Doe', 'jane@example.com', 'dummyhashjane', 'registered', '/images/janedoe.png');
 
